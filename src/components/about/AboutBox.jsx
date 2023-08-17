c35a9185a4ee59ffdf66f9ef9d0c728342b73b32
Henrique Caeiro <henrica2012@gmail.com>
2023-08-17 15:04:12 -0300

    Atualizando projeto

--- src/components/about/AboutBox.jsx
+++ src/components/about/AboutBox.jsx
@@ -7,7 +7,7 @@ const AboutBox = () => {
             <i className="about__icon icon-fire"></i>
 
             <div>
-                <h3 className="about__title">2</h3>
+                <h3 className="about__title">3</h3>
                 <span className="about__subtitle">Projetos completos</span>
             </div>
         </div>

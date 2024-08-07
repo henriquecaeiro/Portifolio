52535382be9c9e433c2d1bc433f7822b6ee08ecc
henriquecaeiro <103581501+henriquecaeiro@users.noreply.github.com>
2024-08-07 14:09:36 -0300

    Netlify error

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-        <a href="https://www.linkedin.com/in/henrique-caeiro-a28135269/" className='home__social-link' target="_blank">
+        <a href="https://www.linkedin.com/in/henrique-caeiro-a28135269/" className='home__social-link' target="_blank" rel="noreferrer">
         <i class="fa-brands fa-linkedin"></i>
         </a>
-        <a href="https://github.com/henriquecaeiro?tab=repositories" className='home__social-link' target="_blank">
+        <a href="https://github.com/henriquecaeiro?tab=repositories" className='home__social-link' target="_blank" rel="noreferrer">
         <i class="fa-brands fa-github"></i>
         </a>
     </div>
--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -37,7 +37,7 @@ const Portifolio = () =>{
 
                             <span className="work__category">{category}</span>
                             <h3 className="work__title">{title}</h3>
-                            <a href={link} target="_blank" className="work__button">
+                            <a href={link} target="_blank" rel="noreferrer" className="work__button">
                                 <i className="icon-link work__button-icon"></i>
                             </a>
                         </div>

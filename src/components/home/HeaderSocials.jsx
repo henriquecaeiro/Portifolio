8ecd7ffdb380dc945201893708f8176621304a48
henriquecaeiro <103581501+henriquecaeiro@users.noreply.github.com>
2024-08-07 13:46:24 -0300

    Atualizando URLs

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-        <a href="https://www.linkedin.com/in/henrique-caeiro-686392200/" className='home__social-link' target="_blank">
+        <a href="https://www.linkedin.com/in/henrique-caeiro-a28135269/" className='home__social-link' target="_blank">
         <i class="fa-brands fa-linkedin"></i>
         </a>
         <a href="https://github.com/henriquecaeiro?tab=repositories" className='home__social-link' target="_blank">
